2f67717088b6375dbbee04fec3d6b3f6e5781c6a
fwyr <fwyr@users.noreply.github.com>
2022-09-03 17:45:03 +0800

    dev: Remove Vercel files, add new model

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, url_for, request, jsonify
 
 # import libraries for AI model
 import numpy as np
-
 import re
 import nltk
 from nltk.corpus import stopwords
@@ -17,76 +16,57 @@ import tensorflow as tf
 from tensorflow import keras
 from keras.preprocessing.text import one_hot
 
-
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
-    return render_template('home.html')
+    if request.method == 'GET':
+        nfn_model = tf.keras.models.load_model('model/nfn_model.h5')
+        data = request.args.get('message')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    nfn_model = tf.keras.models.load_model('model/nfn_model.h5')
-    
-    if request.method == 'POST':
-        message = request.form['message']
-        data = [message][0]
+        if data is None:
+            return jsonify({
+                "error": "Invalid data"
+            })
 
         # preprocess text
         data = data.lower()
         data = data.replace('https://', ' ')
-        data = data.replace('http://', ' ')
         data = re.sub('[^a-zA-Z0-9 ]', ' ', data)
         data = " ".join(data.split())
 
-        stop_words = set(stopwords.words("english"))
         lemmatizer = WordNetLemmatizer()
-
-        filtered = []
-        for word in data.split():
-            if word not in stop_words:
-                filtered.append(word)
-        
-        data = " ".join(filtered)
         data = ["".join([lemmatizer.lemmatize(word) for word in data])]
 
-        one_hot_encoded = [one_hot(text, 10000) for text in data]
-        m = max([len(text) for text in one_hot_encoded])
+        ohe = [one_hot(text, 10000) for text in data]
 
         emb_doc = tf.keras.preprocessing.sequence.pad_sequences(
-            one_hot_encoded,
-            maxlen=5069,
+            ohe,
+            maxlen=5292,
             padding="pre"
         )
 
         pred = nfn_model.predict(np.array(emb_doc))
         percentage = float(pred[0][0])
 
-        verdict, certainty = 0, 0
+        verdict, certainty = "", 0
 
-        if percentage > 0.80:
+        if percentage > 0.8:
             verdict = "Fake"
-            certainty = f"{round(percentage * 100, 2)}%"
-        elif percentage > 0.60:
+        elif percentage > 0.6:
             verdict = "Likely Fake"
-            certainty = f"{round(percentage * 100, 2)}%"
-        elif percentage > 0.:
-            verdict = "Unsure"
-            certainty = f"{round(percentage * 100, 2)}%"
-        elif percentage > 0.20:
+        elif percentage > 0.4:
+            verdict = "Mixed"
+        elif percentage > 0.2:
             verdict = "Likely Real"
-            certainty = f"{round(percentage * 100, 2)}%"
         else:
             verdict = "Real"
-            certainty = f"{round(percentage * 100, 2)}%"
-            
+    
     return jsonify({
         "verdict": verdict,
-        "certainty": certainty,
-        "prediction": percentage,
-        "processed_text": repr(data)
+        "probability": percentage,
+        "text": repr(data)
     })
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
