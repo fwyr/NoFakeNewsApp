324847250542788d9f2a96a04b9c972cfa2a5560
fwyr <fwyr@users.noreply.github.com>
2022-09-13 18:11:01 +0800

    dev: Implement Word2Vec model

---
+++ _site/README.md
@@ -0,0 +1,25 @@
+# NoFakeNews (NFN)
+
+NoFakeNews is an AI-powered browser extension that detects fake news.
+
+This repository contains the code for the back-end parts of NoFakeNews, where we implement a NLP artificial intelligence model into a deployed Flask web application so that necessary data can be easily sent and retrieved through the browser extension.
+
+[NoFakeNewsExt](https://github.com/objecteds/NoFakeNewsExt) is the repository that contains the code for the front-end browser extension.
+
+Contents
+=========
+
+* [Why?](#why)
+* [Project Status](#project-status)
+
+
+### Why?
+
+Fake news can be extremely malicious, inculcating misinformation into those who believe it. Amid the COVID-19 pandemic, fake news has spread rapidly within many communities. As more people pay attention to news, during the pandemic, the general population is more susceptible to the intake of fake news. 
+
+Therefore, NoFakeNews was created to prevent the spreading of fake news and improve the quality of social perspectives held by social citzens. 
+
+
+### Project Status
+
+NFN is extremely early in development. As such, relevant documentation and functionality may not be available. Nonetheless, development is still ongoing.
--- app.py
+++ app.py
@@ -1,27 +1,35 @@
 from flask import Flask, render_template, url_for, request, jsonify
 
-# import libraries for AI model
-import numpy as np
-import re
+# import modules
+import io, json
 import nltk
 from nltk.corpus import stopwords
-from nltk.corpus import wordnet
-from nltk.stem import WordNetLemmatizer
-
 nltk.download('stopwords')
-nltk.download('wordnet')
 nltk.download('omw-1.4')
- 
+nltk.download('punkt')
+stop_words = set(stopwords.words("english"))
+nltk_tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
 import tensorflow as tf
+import gensim
 from tensorflow import keras
-from keras.preprocessing.text import one_hot
+from keras_preprocessing.text import Tokenizer, tokenizer_from_json
+from keras_preprocessing.sequence import pad_sequences
 
+# load app
 app = Flask(__name__)
 
+# load model
+nfn_model = tf.keras.models.load_model('model/nfn_model_w2v.h5')
+
+# load tokenizer
+with open('model/nfn_tokenizer.json') as f:
+    tokenizer_json = json.load(f)
+    nfn_tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(tokenizer_json)
+
+# define app routes
 @app.route('/', methods=['GET'])
 def home():
     if request.method == 'GET':
-        nfn_model = tf.keras.models.load_model('model/nfn_model.h5')
         data = request.args.get('message')
 
         if data is None:
@@ -30,43 +38,35 @@ def home():
             })
 
         # preprocess text
-        data = data.lower()
-        data = data.replace('https://', ' ')
-        data = re.sub('[^a-zA-Z0-9 ]', ' ', data)
-        data = " ".join(data.split())
+        filtered = []
+        data = nltk.sent_tokenize(data)
+        for sentence in data:
+            sentence = sentence.lower()
+            tokens = nltk_tokenizer.tokenize(sentence)
+            filtered_words = [w.strip() for w in tokens if w not in stop_words and len(w) > 1]
+            filtered.extend(filtered_words)
+        data = nfn_tokenizer.texts_to_sequences([filtered])
+        data = pad_sequences(data, maxlen=700)
 
-        lemmatizer = WordNetLemmatizer()
-        data = ["".join([lemmatizer.lemmatize(word) for word in data])]
+        score = nfn_model.predict(data)[0][0]
+        verdict = ""
 
-        ohe = [one_hot(text, 10000) for text in data]
-
-        emb_doc = tf.keras.preprocessing.sequence.pad_sequences(
-            ohe,
-            maxlen=5292,
-            padding="pre"
-        )
-
-        pred = nfn_model.predict(np.array(emb_doc))
-        percentage = float(pred[0][0])
-
-        verdict, certainty = "", 0
-
-        if percentage > 0.8:
-            verdict = "Fake"
-        elif percentage > 0.6:
-            verdict = "Likely Fake"
-        elif percentage > 0.4:
-            verdict = "Mixed"
-        elif percentage > 0.2:
-            verdict = "Likely Real"
-        else:
+        if score <= 0.2:
             verdict = "Real"
-    
-    return jsonify({
-        "verdict": verdict,
-        "probability": percentage,
-        "text": repr(data)
-    })
+        elif score <= 0.4:
+            verdict = "Likely Real"
+        elif score <= 0.6:
+            verdict = "Mixed"
+        elif score <= 0.8:
+            verdict = "Likely Fake"
+        else:
+            verdict = "Fake"
+
+        return jsonify({
+            "verdict": verdict,
+            "score": str(score),
+            "processed_text": repr(filtered)
+        })
 
 if __name__ == '__main__':
     app.run(debug=True)
